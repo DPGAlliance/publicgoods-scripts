6fb3429a91f44d24569836593267558e6aae4066
Victor Grau Serrat <lacabra@users.noreply.github.com>
2019-08-15 17:12:20 +0200

    BLD: removed wordpress versioning of css/js files

--- static.bash
+++ static.bash
@@ -1,6 +1,20 @@
 #!/bin/bash
+
+# source a static version of the site
 wget --no-host-directories --recursive --page-requisites --no-parent --timestamping -P static http://localhost:8000
+
+# cd into the folder
 pushd static
+
+# remove unneeded files
+rm -rf comments feed wp-json xmlrpc.php\?rsd wp-login* wp-admin author index.html\?*
+
+for file in $(find . -name "*\?*")
+do
+	echo $file
+	mv $file $(echo $file | sed s/\?.*$//)
+done
+
 if [[ "$OSTYPE" == "darwin"* ]]; then
 	echo 'Detected OSX...'
 	find . -name '*.html' -exec sed -i '' -e 's_http://localhost:8000/_/_g' {} \;
@@ -10,7 +24,6 @@ else
 	find . -name '*.html' -exec sed -i -e 's_http://localhost:8000/_/_g' {} \;
 	find . -name '*.html' -exec sed -i -e 's_http:\\/\\/localhost:8000\\/_\\/_g' {} \;
 fi
-rm -rf comments feed wp-json xmlrpc.php\?rsd wp-login* wp-admin author
 popd
 cp -a static/* ../publicgoods-website
 rm -rf static
