bdad8ece9595f6c7bec072a59cd24c2f00a94db5
Victor Grau Serrat <lacabra@users.noreply.github.com>
2020-04-15 16:32:42 -0600

    BLD: moveFiles.bash returns true iff all cmds succeed

--- moveFiles.bash
+++ moveFiles.bash
@@ -1,8 +1,8 @@
 #/bin/bash
-pushd ../publicgoods-website/explore
-git rm -r static/*
-mkdir static
-cp -a ../../publicgoods-scripts/build/static/* static
-git add static/*
-cp ../../publicgoods-scripts/build/index.html .
-popd
+pushd ../publicgoods-website/explore && \
+	git rm -r static/* && \
+    mkdir static && \
+    cp -a ../../publicgoods-scripts/build/static/* static && \
+    git add static/* && \
+    cp ../../publicgoods-scripts/build/index.html . && \
+    popd
