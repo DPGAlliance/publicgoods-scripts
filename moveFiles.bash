03314a370195e8ba04f80968e6528f9d6c5ed477
Victor Grau Serrat <lacabra@users.noreply.github.com>
2020-04-15 17:37:21 -0600

    BUG: moveFiles more robust, handle missing static folder

--- moveFiles.bash
+++ moveFiles.bash
@@ -1,6 +1,6 @@
 #/bin/bash
 pushd ../publicgoods-website/explore && \
-	git rm -r static/* && \
+	git rm -rf static/* || true && \
     mkdir static && \
     cp -a ../../publicgoods-scripts/build/static/* static && \
     git add static/* && \
