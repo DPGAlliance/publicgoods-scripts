591d6ac447b06da052ded619f58350c4ceea8869
Nathan Kwabena Fletcher <fletcher.nathan.a@gmail.com>
2021-07-21 14:00:16 +0000

    updated font sym link for testing on CI

--- scripts/moveFiles.bash
+++ scripts/moveFiles.bash
@@ -6,10 +6,10 @@ pushd ../publicgoods-website/registry && \
     git add static/* && \
     cp ../../publicgoods-scripts/packages/registry/build/index.html . && \
 popd
-# pushd ../publicgoods-website/eligibility && \
-#     cp -R ../../publicgoods-website/wp-content/themes/dpga/font/. ../../publicgoods-scripts/packages/eligibility/src/font/ && \
-# popd
-# pushd packages/eligibility && npm install && npm run build && popd
+pushd ../publicgoods-website/eligibility && \
+    cp -R ../../publicgoods-website/wp-content/themes/dpga/font/. ../../publicgoods-scripts/packages/eligibility/src/font/ && \
+popd
+pushd packages/eligibility && npm install && npm run build && popd
 pushd ../publicgoods-website/eligibility && \
 	git rm -rf static/* || true && \
     mkdir static && \
