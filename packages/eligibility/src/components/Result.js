f7c6b2297ac974e891d5b031fb7751208cf61df1
Rachita Saha <rachita19082@iiitd.ac.in>
2021-08-10 18:49:38 +0530

    Update user workflow

--- packages/eligibility/src/components/Result.js
+++ packages/eligibility/src/components/Result.js
@@ -5,22 +5,22 @@ import Summary from './Summary'
 function Result(props) {
   return (
     <div className="result">
-    {props.quizScore === 9 && (
+    {props.quizScore === props.total && props.total === 9 && (
       <>
-      <h2 className="text-center"> You are eligible! </h2>
+      <h2 className="text-center"> Your digital solution is eligible! </h2>
       <div className="pt-3 pl-4 pr-4 pb-4">
-        You correctly answered <strong>{props.quizScore}/9</strong> in the Eligibility Test. Therefore, you qualify
+        You correctly answered <strong>{props.quizScore}/{props.total}</strong> in the Eligibility Test. Therefore, you qualify
         to nominate your digital solution to DPG. Click <strong>Proceed</strong> to complete the Nomination Form! <br></br>
         Have questions? Contact us <a href="mailto:%20hello@digitalpublicgoods.net">here</a>.
       </div>
       </>
     )}
 
-    {props.quizScore < 9 && props.questions.length === 0 && props.maybeQuestions.length > 0 && (
+    {props.quizScore <= props.total && props.questions.length === 0 && props.maybeQuestions.length > 0 && (
       <>
-      <h2 className="text-center"> You may be eligible! </h2>
+      <h2 className="text-center"> Your digital solution may be eligible! </h2>
       <div className="pt-3 pl-5 pr-4 pb-4">
-        You correctly answered <strong>{props.quizScore}/9</strong> in the Eligibility Test. You may be eligible to nominate
+        You correctly answered <strong>{props.quizScore}/{props.total}</strong> in the Eligibility Test. You may be eligible to nominate
         your digital solution to DPG based on the conditions given below. 
       </div>
       <div className="summary">    
@@ -39,11 +39,11 @@ function Result(props) {
       </>
     )}
 
-    {props.quizScore < 9 && props.questions.length > 0 && (
+    {props.quizScore < props.total && props.questions.length > 0 && (
       <>
-      <h2 className="text-center"> Sorry! You are not eligible. </h2>
+      <h2 className="text-center"> Sorry! Your digital solution is not eligible. </h2>
       <div className="pt-3 pl-4 pr-4 pb-4">
-        You correctly answered <strong>{props.quizScore}/9</strong> in the Eligibility Test. The digital solution you are considering to nominate is not currently eligible to become a DPG since you must correctly answer <strong>9/9</strong> in order to be eligible. 
+        You correctly answered <strong>{props.quizScore}/{props.total}</strong> in the Eligibility Test. The digital solution you are considering to nominate is not currently eligible to become a DPG since you must correctly answer <strong>{props.total}/{props.total}</strong> in order to be eligible. 
         Take a look at the responses you provided and a summary of the changes required before becoming a DPG.
       </div>
       <div className="summary">    
