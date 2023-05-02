4bb62142d41c521e3049b2e59b488a3c828e6101
prashansatw <prashansa.saxena@thoughtworks.com>
2023-05-02 16:28:56 +0530

    [Prashansa/Ruchith/Swetha] Adding headless chrome browser

--- TestAutomation/DPGAUIAutomation/src/test/java/utilities/BrowserConfig.java
+++ TestAutomation/DPGAUIAutomation/src/test/java/utilities/BrowserConfig.java
@@ -14,18 +14,32 @@ public class BrowserConfig {
     public static void setUpDriver(){
 
 
-       /*
-       WebDriverManager.safaridriver().setup();
-       driver = new SafariDriver();
-*/
+    
+    //    WebDriverManager.safaridriver().setup();
+    //    driver = new SafariDriver();
 
 
 
 
-        WebDriverManager.chromedriver().setup();
-        ChromeOptions co= new ChromeOptions();
-        co.addArguments("--remote-allow-origins=*");
-        driver = new ChromeDriver(co);
+
+        // WebDriverManager.chromedriver().setup();
+        // ChromeOptions co= new ChromeOptions();
+        // co.addArguments("--remote-allow-origins=*");
+        // driver = new ChromeDriver(co);
+
+String downloadFilepath = System.getProperty("user.dir");
+ WebDriverManager.chromedriver().setup();
+ ChromeOptions chromeOptions;
+ chromeOptions = new ChromeOptions();
+ HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
+ chromePrefs.put("profile.default_content_settings.popups", 0);
+ chromePrefs.put("download.default_directory", downloadFilepath);
+ chromeOptions.addArguments("--no-sandbox");
+ chromeOptions.addArguments("--disable-dev-shm-usage");
+ chromeOptions.addArguments("--window-size=1325x744");
+ chromeOptions.addArguments("--headless");chromeOptions.setExperimentalOption("prefs", chromePrefs);
+ driver = new ChromeDriver(chromeOptions);
+ driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 
 
 
