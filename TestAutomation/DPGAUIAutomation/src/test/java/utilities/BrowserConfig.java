7c2bd02fd82c663d06d1387415cf5cc7e96b39d7
swetha.mukthapuram <swetha.mukthapuram@swethamukthapurams-MacBook-Pro.local>
2023-05-23 09:56:07 +0530

    [Swetha] Test added

--- TestAutomation/DPGAUIAutomation/src/test/java/utilities/BrowserConfig.java
+++ TestAutomation/DPGAUIAutomation/src/test/java/utilities/BrowserConfig.java
@@ -22,49 +22,28 @@ public class BrowserConfig {
 
 
 
-        // WebDriverManager.chromedriver().setup();
-        // ChromeOptions co= new ChromeOptions();
-        // co.addArguments("--remote-allow-origins=*");
-        // driver = new ChromeDriver(co);
+         /*WebDriverManager.chromedriver().setup();
+         ChromeOptions co= new ChromeOptions();
+         co.addArguments("--remote-allow-origins=*");
+         driver = new ChromeDriver(co);*/
 
-String downloadFilepath = System.getProperty("user.dir");
- WebDriverManager.chromedriver().setup();
- ChromeOptions chromeOptions;
- chromeOptions = new ChromeOptions();
- HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
- chromePrefs.put("profile.default_content_settings.popups", 0);
- chromePrefs.put("download.default_directory", downloadFilepath);
- chromeOptions.addArguments("--no-sandbox");
- chromeOptions.addArguments("--disable-dev-shm-usage");
- chromeOptions.addArguments("--window-size=1325x744");
- chromeOptions.addArguments("--headless");chromeOptions.setExperimentalOption("prefs", chromePrefs);
- driver = new ChromeDriver(chromeOptions);
- driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-
-
+         String downloadFilepath = System.getProperty("user.dir");
+         WebDriverManager.chromedriver().setup();
+         ChromeOptions chromeOptions;
+         chromeOptions = new ChromeOptions();
+         HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
+         chromePrefs.put("profile.default_content_settings.popups", 0);
+         chromePrefs.put("download.default_directory", downloadFilepath);
+         chromeOptions.addArguments("--no-sandbox");
+         chromeOptions.addArguments("--disable-dev-shm-usage");
+         chromeOptions.addArguments("--window-size=1325x744");
+         chromeOptions.addArguments("--headless");chromeOptions.setExperimentalOption("prefs", chromePrefs);
+         driver = new ChromeDriver(chromeOptions);
+         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 
 
-        //headless chrome
-        /*String downloadFilepath = System.getProperty("user.dir");
-        WebDriverManager.chromedriver().setup();
-        ChromeOptions chromeOptions;
-        chromeOptions = new ChromeOptions();
-        HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
-        chromePrefs.put("profile.default_content_settings.popups", 0);
-        chromePrefs.put("download.default_directory", downloadFilepath);
-        chromeOptions.addArguments("--headless");
-        chromeOptions.addArguments("--whitelisted-ips");
-        chromeOptions.addArguments("--no-sandbox");
-        chromeOptions.addArguments("--disable-extensions");
-        chromeOptions.addArguments("--remote-allow-origins=*");
-        chromeOptions.addArguments("--headless", "--disable-gpu","--ignore-certificate-errors", "--disable-dev-shm-usage");
-        chromeOptions.setExperimentalOption("prefs", chromePrefs);
-        driver = new ChromeDriver(chromeOptions);
-        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS); */
-        //ChromeOptions opts = new ChromeOptions();
-        //opts.setExperimentalOption("prefs", prefs);
-        //
-        //driver = new ChromeDriver(opts);
+
+
     }
 
     public static void quitDriver() {
