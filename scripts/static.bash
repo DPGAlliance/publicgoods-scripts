e73ccd8d0d35d36f43b7ae748399094327bfa185
law909 <balint.lovey@gmail.com>
2023-11-07 02:48:27 +0100

    heroku -> private server

--- scripts/static.bash
+++ scripts/static.bash
@@ -3,7 +3,7 @@
 # DOMAIN=" https://dpgalliance.github.io/"
 
 # source a static version of the site
-wget --no-host-directories --recursive --page-requisites --no-parent --timestamping -e robots=off -w 1 --random-wait --retry-on-http-error=503 -P static http://dpgwebsite.herokuapp.com http://dpgwebsite.herokuapp.com/submission-guide/
+wget --no-host-directories --recursive --page-requisites --no-parent --timestamping -e robots=off -w 1 --random-wait --retry-on-http-error=503 -P static http://sourcewp.digitalpublicgoods.net http://sourcewp.digitalpublicgoods.net/submission-guide/
 
 # cd into the folder
 pushd static
@@ -19,22 +19,22 @@ done
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
 	echo 'Detected OSX...'
-	find . -name '*.html' -exec sed -i '' -e 's_http://dpgwebsite.herokuapp.com/_/_g' {} \;
-	find . -name '*.html' -exec sed -i '' -e 's_https://dpgwebsite.herokuapp.com/_/_g' {} \;
-	find . -name '*.html' -exec sed -i '' -e 's_http:\\/\\/dpgwebsite.herokuapp.com\\/_\\/_g' {} \;
-	find . -name '*.html' -exec sed -i '' -e 's_https:\\/\\/dpgwebsite.herokuapp.com\\/_\\/_g' {} \;
+	find . -name '*.html' -exec sed -i '' -e 's_http://sourcewp.digitalpublicgoods.net/_/_g' {} \;
+	find . -name '*.html' -exec sed -i '' -e 's_https://sourcewp.digitalpublicgoods.net/_/_g' {} \;
+	find . -name '*.html' -exec sed -i '' -e 's_http:\\/\\/sourcewp.digitalpublicgoods.net\\/_\\/_g' {} \;
+	find . -name '*.html' -exec sed -i '' -e 's_https:\\/\\/sourcewp.digitalpublicgoods.net\\/_\\/_g' {} \;
 	# After all URLs with page links after the / have been replaced,
 	# anything without trailing / is replaced with the actual domain
-	# find . -name '*.html' -exec sed -i '' -e "s,http://dpgwebsite.herokuapp.com,$DOMAIN,g" {} \;
+	# find . -name '*.html' -exec sed -i '' -e "s,http://sourcewp.digitalpublicgoods.net,$DOMAIN,g" {} \;
 else
 	echo 'Detected non-OSX...'
-	find . -name '*.html' -exec sed -i -e 's_http://dpgwebsite.herokuapp.com/_/_g' {} \;
-	find . -name '*.html' -exec sed -i -e 's_https://dpgwebsite.herokuapp.com/_/_g' {} \;
-	find . -name '*.html' -exec sed -i -e 's_http:\\/\\/dpgwebsite.herokuapp.com\\/_\\/_g' {} \;
-	find . -name '*.html' -exec sed -i -e 's_https:\\/\\/dpgwebsite.herokuapp.com\\/_\\/_g' {} \;
+	find . -name '*.html' -exec sed -i -e 's_http://sourcewp.digitalpublicgoods.net/_/_g' {} \;
+	find . -name '*.html' -exec sed -i -e 's_https://sourcewp.digitalpublicgoods.net/_/_g' {} \;
+	find . -name '*.html' -exec sed -i -e 's_http:\\/\\/sourcewp.digitalpublicgoods.net\\/_\\/_g' {} \;
+	find . -name '*.html' -exec sed -i -e 's_https:\\/\\/sourcewp.digitalpublicgoods.net\\/_\\/_g' {} \;
 	# After all URLs with page links after the / have been replaced,
 	# anything without trailing / is replaced with the actual domain
-	# find . -name '*.html' -exec sed -i -e "s,http://dpgwebsite.herokuapp.com,$DOMAIN,g" {} \;
+	# find . -name '*.html' -exec sed -i -e "s,http://sourcewp.digitalpublicgoods.net,$DOMAIN,g" {} \;
 fi
 popd
 cp -a static/* ../publicgoods-website-test
